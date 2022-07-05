56b9c467f38c24b8d2722d8426e6ece6b2e215b0
ivan.stefanov <ivanstefanov.it@gmail.com>
2022-07-05 12:02:47 +0300

    Add project files.

---
+++ ImageUploadAndRetrieve/Controllers/FileUploadsController.cs
@@ -0,0 +1,61 @@
+﻿using ImageUploadAndRetrieve.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImageUploadAndRetrieve.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileUploadsController : ControllerBase
+    {
+        private readonly IWebHostEnvironment webHostEnvironment;
+
+        public FileUploadsController(IWebHostEnvironment webHostEnvironment)
+        {
+            this.webHostEnvironment = webHostEnvironment;
+        }
+
+        [HttpPost]
+        public async Task<string> Post([FromForm] FileUpload fileUpload)
+        {
+            try
+            {
+                if (fileUpload.files.Length > 0)
+                {
+                    string path = this.webHostEnvironment.WebRootPath + "\\uploads\\";
+                    if (!Directory.Exists(path))
+                    {
+                        Directory.CreateDirectory(path);
+                    }
+                    using (FileStream fileStream = System.IO.File.Create(path + fileUpload.files.FileName))
+                    {
+                        fileUpload.files.CopyTo(fileStream);
+                        fileStream.Flush();
+                        return "Upload Done";
+                    }
+                }
+                else
+                {
+                    return "Failed.";
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        [HttpGet("{fileName}")]
+        public async Task<IActionResult> Get([FromRoute] string fileName)
+        {
+            string path = this.webHostEnvironment.WebRootPath + "\\uploads\\";
+            var filePath = path + fileName + ".png";
+            if (System.IO.File.Exists(filePath))
+            {
+                byte[] b = System.IO.File.ReadAllBytes(filePath);
+                return File(b, "image/png");
+            }
+            return null;
+        }
+    }
+}
